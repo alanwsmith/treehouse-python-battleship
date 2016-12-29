74d62b909a176aac270fa928098977d55aebe5e5
Alan W. Smith <alan@alanwsmith.com>
2016-12-29 14:30:11 -0500

    Replaced deprecated arena_display with the new format.

--- game.py
+++ game.py
@@ -16,7 +16,7 @@ class Game():
     def arena_padding(self):
         return "      "
 
-    def display_arena_2(self):
+    def display_arena(self):
         print(
             " {} ".format(self.boards[0].player_name).center(22, '-')
             +
@@ -41,33 +41,6 @@ class Game():
         else:
             print('\n\n')
 
-        
-    def display_arena(self, **kwargs):
-        self.display_board(self.boards[kwargs['top_board']])
-        print('')
-        if kwargs['top_board'] == 0:
-            self.display_board(self.boards[1])
-        else:
-            self.display_board(self.boards[0])
-
-        # The flash_message only appears once and is then
-        # removed. If there is no flash_message, empty
-        # lines are output to maintain the spacing. 
-        if self.flash_message != "":
-            print('\n{}\n'.format(self.flash_message))
-            self.flash_message = ""
-        else:
-            print('\n\n')
-
-
-    def display_board(self, board):
-        print(" {} ".format(board.player_name).center(22, '-'))
-        print("   " + " ".join([chr(c) for c in range(ord('A'), ord('A') + constants.BOARD_SIZE)]))
-        row_num = 1
-        for row in board.grid():
-            print(str(row_num).rjust(2) + " " + (" ".join(row)))
-            row_num += 1
-
     def place_ships_on_board(self, board):
         logging.info("Placing ships for board {}".format(board.index))
         for ship in board.ships:
@@ -81,8 +54,7 @@ if __name__ == '__main__':
         level=logging.INFO
     )
     game = Game()
-    # game.display_arena(top_board = 1)
-    game.display_arena_2()
+    game.display_arena()
     # game.place_ships_on_board(game.boards[0])
 
         
