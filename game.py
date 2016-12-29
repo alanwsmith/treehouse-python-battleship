f15256eb4e4813a599f2327b2bfb55c687b2c63e
Alan W. Smith <alan@alanwsmith.com>
2016-12-29 06:26:55 -0500

    Creating two boards and showing the output for one of them.

--- game.py
+++ game.py
@@ -1,13 +1,25 @@
 import logging
 
+from board import Board
+
 class Game():
     
     def __init__(self):
         logging.info("Initializing game")
+        self.boards = [ Board(), Board() ]
+
+    def display_board(self, board):
+        board.show()
+        pass
+
+
+
+
 
 
 if __name__ == '__main__':
     logging.basicConfig(format='[%(levelname)s]:  %(message)s', level=logging.INFO)
     game = Game()
+    game.display_board(game.boards[0])
 
         
