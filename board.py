487552f3548acc18154320949c1aa91f632f6758
Alan W. Smith <alan@alanwsmith.com>
2016-12-28 23:25:51 -0500

    Created the basic board output based on a two dimensional array.

--- board.py
+++ board.py
@@ -8,8 +8,17 @@ class Board():
         logging.debug("Creating new board")
         pass
 
+    def show(self):
+        print("   " + " ".join([chr(c) for c in range(ord('A'), ord('A') + constants.BOARD_SIZE)]))
+        for row in range(0,10):
+            print(str(row + 1).rjust(2), end='')
+            for col in range(0, 10):
+                print(" O", end='')
+            print('')
+
 
 if __name__ == '__main__':
-    logging.basicConfig(format='[%(levelname)s]:  %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='[%(levelname)s]:  %(message)s', level=logging.INFO)
     board = Board()
+    board.show()
 
