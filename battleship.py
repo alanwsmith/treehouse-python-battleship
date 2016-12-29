1e79f8b38c65f8957d5532533678048a826b3b7f
Alan W. Smith <alan@alanwsmith.com>
2016-12-28 19:58:46 -0500

    Hid some boats for faster testing.

--- battleship.py
+++ battleship.py
@@ -5,10 +5,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 SHIP_INFO = [
     ("Aircraft Carrier", 5),
-    ("Battleship", 4),
-    ("Submarine", 3),
-    ("Cruiser", 3),
-    ("Patrol Boat", 2)
+#    ("Battleship", 4),
+#    ("Submarine", 3),
+#    ("Cruiser", 3),
+#    ("Patrol Boat", 2)
 ]
 
 BOARD_SIZE = 10
