5aeaf84dcfbb558e5faf524d45465413e9023cf9
Alan W. Smith <alan@alanwsmith.com>
2016-12-29 00:51:10 -0500

    Setup 'columns' which will replace 'cols' in COORDINATE_MAP.

--- constants.py
+++ constants.py
@@ -32,8 +32,10 @@ LETTERS = "abcdefghijklmnopqrstuvwxyz"
 
 # This map translates the coordinates that a player enters into
 # the coordinates used internally to draw the board. 
+
 COORDINATE_MAP = {
     'cols': dict(zip(LETTERS[:BOARD_SIZE], range(0,BOARD_SIZE))),
+    'columns': dict(zip(LETTERS[:BOARD_SIZE], range(0,BOARD_SIZE))),
     'rows': dict(zip(range(1, BOARD_SIZE + 1), range(0, BOARD_SIZE))) 
 }
 
