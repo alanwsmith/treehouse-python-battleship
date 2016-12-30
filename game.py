b4a8e6524eda10b380ff2e70d52b76e57ad4245a
Alan W. Smith <alan@alanwsmith.com>
2016-12-30 13:11:38 -0500

    Moved GameTest into its own file.

--- game.py
+++ game.py
@@ -69,49 +69,17 @@ class Game():
         pass
 
 
-class GameTest():
-    def __init__(self):
-        logging.info("Initialized GameTest()")
-
-    def assert_equal(self, a, b):
-        if a != b:
-            raise ValueError("Expected: {} - Got: {}".format(a, b))
-
-    def run_tests(self):
-        logging.info("Running tests.")
-        self.test_basic_initialization()
-        self.test_2()
-
-    def test_basic_initialization(self):
-        logging.info("-- Test 1 - Started --")
-        game = Game()
-        self.assert_equal(game.boards[0].player_name, "Player 1")
-        self.assert_equal(game.boards[1].player_name, "Player 2")
-        self.assert_equal(game.message_key, "welcome")
-        logging.info("-- Test 1 - Finished --")
-
-    def test_2(self):
-        logging.info("-- Test 2 - Started --")
-        game = Game()
-        game.display_arena()
-        game.get_player_name_for_board(board_index=0, test_value="Bob")
-        self.assert_equal(game.boards[0].player_name, "Bob")
-        logging.info("-- Test 2 - Finished --")
-        
 
 if __name__ == '__main__':
     logging.basicConfig(
-        filename='logs/test-run--board.txt', 
+        filename='logs/game.txt', 
         filemode='w', 
         format='[%(levelname).1s]: %(message)s', 
         level=logging.INFO
     )
 
-    gt = GameTest()
-    gt.run_tests()
-
-    #game = Game()
-    #game.display_arena()
+    game = Game()
+    game.display_arena()
     #game.get_player_name_for_board(board_index=0)
 #    game.get_player_names()
 
