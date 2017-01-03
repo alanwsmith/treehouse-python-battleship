4a11d7caa37fba666151038ab497619ba84e8fc0
Alan W. Smith <alan@alanwsmith.com>
2017-01-02 21:22:04 -0500

    Removed deprecated place method.

--- ship.py
+++ ship.py
@@ -31,68 +31,6 @@ class Ship():
         self.orientation = orientation
 
 
-    def place(self):
-
-        valid_orientations = ["v", "h"]
-        
-        # Figure out the orientation
-        while True:
-            response = input("[v]ertically or [h]orizontally? ").lower()
-            if response in valid_orientations:
-                self.orientation = response
-                break
-            else:
-                print("Oops! You must enter either 'v' for vertical or 'h' for horizontal. Try again.")
-                continue
-
-        # Get the bow coordinates
-
-        last_letter = constants.LETTERS[constants.BOARD_MAX_INDEX].upper()
-        last_number = constants.BOARD_SIZE 
-
-        while True:
-            column = input("Which column (A-{})? ".format(last_letter)).lower()
-            if column in constants.COORDINATE_MAP['cols']:
-                while True:
-                    row = input("Which row (1-{})? ".format(last_number)) 
-                    try:
-                        row = int(row)
-                    except ValueError:
-                        print("Oops! The row must be a number. Try again.")
-                        continue
-                    else:
-                        if row in constants.COORDINATE_MAP['rows']:
-                            self.bow = (
-                                constants.COORDINATE_MAP['rows'][row],
-                                constants.COORDINATE_MAP['cols'][column]
-                            )
-
-                            break
-                        else:
-                            print("Oops! {} is outside the grid. Give it another shot.".format(row))
-                            continue
-                break
-            else:
-                print("Oops! That wasn't a valid row. Try one more time")
-                continue
-            break
-        #     else:
-        #         while True:
-        #             target_row = input("Which row (1-{})? ".format(last_number)) 
-        #             try:
-        #                 target_row = int(target_row)
-        #             except ValueError:
-        #                 print("Oops! The row must be a number. Give it another shot.")
-        #                 continue
-        #             if target_row not in constants.COORDINATE_MAP['rows']:
-        #                 print("Oops! '{}' is not a valid row. Give it another shot.".format(target_row))
-        #                 continue
-        #             else:
-        #                 return(target_column, target_row)
-
-        # pass
-
-
 if __name__ == '__main__':
     ship = Ship(name = "Test Ship", size = 5)
     ship.place()
