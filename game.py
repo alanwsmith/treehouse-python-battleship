bc0f9db2c1058c4eac4c2b255b84449d8655175b
Alan W. Smith <alan@alanwsmith.com>
2017-01-04 11:47:00 -0500

    Moved comment in line for testing_input setup.

--- game.py
+++ game.py
@@ -389,8 +389,7 @@ if __name__ == '__main__':
     game = Game()
     game.testing_input = ["Bob", "John"]
     game.testing_input = ["Bob", "John", "b3", "v", "d2", "h", "i6", "v", "", "a1", "v", "b1", "v", "c1", "v", ""]
-    # This is where shots start being fired.
-    game.testing_input.extend(["a1", "", "a1", ""])
+    game.testing_input.extend(["a1", "", "a1", ""]) # Start firing shots.
     game.set_player_names()
     game.place_ships()
     game.set_current_player(0)
