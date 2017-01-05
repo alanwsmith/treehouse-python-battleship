df73f3871972b3722d0d08922af5df32f6f9ceb8
Alan W. Smith <alan@alanwsmith.com>
2017-01-04 19:58:26 -0500

    Showing the name of the ship that was just sunk.

--- game.py
+++ game.py
@@ -383,7 +383,7 @@ class Game():
             player_board.set_grid_visibility(False)
             self.current['last_shot'] = self.raw_coordinates_to_display(opponents_board.last_shot())
             self.banner = opponents_board.last_shot_status()
-            self.current['ship'] = "PLACEHOLDER"
+            self.current['ship'] = opponents_board.get_name_of_ship_that_was_just_hit() 
             self.prompt = "continue"
             self.display_arena()
             self.get_input()
